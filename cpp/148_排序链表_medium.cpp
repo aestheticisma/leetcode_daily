352601dda0610e5084e30fa31f89ff2422789498
aestheticisma <1071191953@qq.com>
2020-11-22 00:31:45 +0800

    update 148_排序链表_medium

--- cpp/148_排序链表_medium.cpp
+++ cpp/148_排序链表_medium.cpp
@@ -59,3 +59,81 @@ public:
         return dummyHead->next;
     }
 };
+
+// 法2：自底向上 先将链表分为n份 之后进行两两合并
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(head==nullptr)
+            return head;
+        int length = 0;
+        ListNode *node = head;
+        while(node!=nullptr){
+            node = node->next;
+            length++;
+        }
+        ListNode *dummyHead = new ListNode(0, head);
+        // 当subLength大于等于链表总长度时停止
+        for(int subLength=1; subLength<length; subLength<<=1){
+            ListNode *current=dummyHead->next;
+            ListNode *pre=dummyHead;
+            // 循环两两合并
+            while(current!=nullptr){
+                ListNode *head1 = current;
+                // 移动current指针到第一组链表尾部
+                for(int i=1; i<subLength && current->next!=nullptr; i++)
+                    current = current->next;
+                ListNode *head2 = current->next;
+                current->next=nullptr;
+                current = head2;
+                // 移动current指针到第二组链表尾部
+                for(int i=1; i<subLength && current!=nullptr && current->next!=nullptr; i++)
+                    current = current->next;
+                //如果存在后续单元，则将current指针后移至下一位并断开第二组与之后的链接
+                if(current!=nullptr){
+                    ListNode *third = nullptr;
+                    third = current->next;
+                    current->next = nullptr;
+                    current = third;
+                }
+                pre->next = merge(head1, head2);
+                // 移动pre至下两个将要合并的位置
+                while(pre->next!=nullptr)
+                    pre = pre->next;
+                // current = third;
+            }
+        }
+        return dummyHead->next;
+    }
+
+    ListNode* merge(ListNode* head1, ListNode* head2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* temp = dummyHead, *temp1 = head1, *temp2 = head2;
+        while (temp1 != nullptr && temp2 != nullptr) {
+            if (temp1->val <= temp2->val) {
+                temp->next = temp1;
+                temp1 = temp1->next;
+            } else {
+                temp->next = temp2;
+                temp2 = temp2->next;
+            }
+            temp = temp->next;
+        }
+        if (temp1 != nullptr) {
+            temp->next = temp1;
+        } else if (temp2 != nullptr) {
+            temp->next = temp2;
+        }
+        return dummyHead->next;
+    }
+};
--- python/148_排序链表_medium.py
+++ python/148_排序链表_medium.py
@@ -40,3 +40,76 @@ class Solution:
                 temp.next = temp2
             return dummyHead.next
         return divideList(head, None)
+
+
+# 法二：自底向上 先将链表分为n份 之后进行两两合并
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        # 两个有序链表合并函数
+        def merge(head1, head2):
+            dummyHead = ListNode(0)
+            temp, temp1, temp2 = dummyHead, head1, head2
+            while temp1 and temp2:
+                if temp1.val <= temp2.val:
+                    temp.next = temp1
+                    temp1 = temp1.next
+                else:
+                    temp.next = temp2
+                    temp2 = temp2.next
+                temp = temp.next
+            if temp1:
+                temp.next = temp1
+            elif temp2:
+                temp.next = temp2
+            return dummyHead.next
+        # 如果链表为空 返回
+        if not head:
+            return head
+        length = 0
+        node = head
+        # 计算链表长度
+        while node:
+            node = node.next
+            length += 1
+        dummyHead, subLength = ListNode(0), 1
+        dummyHead.next = head
+        # 当subLength大于等于链表总长度时停止
+        while(subLength<length):
+            pre, current = dummyHead, dummyHead.next
+            # 循环两两合并
+            while current:
+                # 存储第一组链表首部
+                head1 = current
+                # 将current指向第一组链表尾部
+                for i in range(1, subLength):
+                    if current.next:
+                        current = current.next
+                    else: break
+                # 存储第二组链表首部
+                head2 = current.next
+                # 断开第一组链表
+                current.next = None
+                current = head2
+                # 将current指向第二组链表尾部(如果第二组链表存在的话)
+                for i in range(1, subLength):
+                    if current and current.next:
+                        current = current.next
+                    else:
+                        break
+                if current:
+                    third = current.next
+                    current.next = None
+                    current = third
+                # 移动pre至下两个将要合并的位置
+                pre.next = merge(head1, head2)
+                while pre.next:
+                    pre = pre.next
+            # subLength变为原来的两倍
+            subLength <<= 1
+        return dummyHead.next
