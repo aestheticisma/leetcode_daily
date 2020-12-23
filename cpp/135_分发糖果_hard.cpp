fd408898ea4cfa3bd652d1071a100a067bdbb04d
aestheticisma <1071191953@qq.com>
2020-12-24 00:56:11 +0800

    update 135_分发糖果_hard

--- cpp/135_分发糖果_hard.cpp
+++ cpp/135_分发糖果_hard.cpp
@@ -1,3 +1,4 @@
+// 法一：
 class Solution {
 public:
     int candy(vector<int>& ratings) {
@@ -20,6 +21,29 @@ public:
                 res += des;
                 pre = 1;
             }
+        }
+        return res;
+    }
+};
+
+//法二：
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int N = ratings.size();
+        vector<int> left(N);
+        for(int i=0; i<N; i++)
+            if(i>0 && ratings[i]>ratings[i-1])
+                left[i] = left[i-1] + 1;
+            else
+                left[i] = 1;
+        int right = 0, res = 0;
+        for(int i=N-1; i>=0; i--){
+            if(i<N-1 && ratings[i]>ratings[i+1])
+                right++;
+            else 
+                right = 1;
+            res += max(left[i], right);
         }
         return res;
     }
--- python/135_分发糖果_hard.py
+++ python/135_分发糖果_hard.py
@@ -1,3 +1,4 @@
+# 法一：
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         ins, des, pre = 1, 0, 1
@@ -15,4 +16,23 @@ class Solution:
                     des += 1
                 res += des
                 pre = 1
+        return res
+
+# 法二：
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        N = len(ratings)
+        left = [0 for i in range(N)]
+        for i in range(N):
+            if i>0 and ratings[i]>ratings[i-1]:
+                left[i] = left[i-1] + 1
+            else:
+                left[i] = 1
+        right, res = 0, 0
+        for i in range(N-1, -1, -1):
+            if i<N-1 and ratings[i]>ratings[i+1]:
+                right += 1
+            else:
+                right = 1
+            res += max(left[i], right)
         return res
