5b7b067194d2733aadd5cbf49b47b5d579075619
aestheticisma <1071191953@qq.com>
2021-12-27 16:42:53 +0800

    sth

--- cpp/825_适龄的朋友_medium.cpp
+++ cpp/825_适龄的朋友_medium.cpp
@@ -1,3 +1,4 @@
+// 排序 + 双指针
 class Solution {
 public:
     int numFriendRequests(vector<int>& ages) {
@@ -9,6 +10,24 @@ public:
             while (ages[left] <= 0.5 * age + 7) ++left;
             while (right + 1 < n && ages[right + 1] <= age) ++right;
             ans += right - left;
+        }
+        return ans;
+    }
+};
+
+// 计数 + 前缀和
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        vector<int> cnt(121);
+        for (const int & age : ages) ++cnt[age];
+        vector<int> pre(121);
+        for (int i = 1; i < 121; ++i)
+            pre[i] = pre[i-1] + cnt[i];
+        int ans = 0;
+        for (int i = 15; i < 121; ++i) {
+            int bound = 0.5 * i + 8;
+            ans += cnt[i] * (pre[i] - pre[bound-1] - 1);
         }
         return ans;
     }
