ebb1677572f9839b34225077e669dc84dc98c8c5
aestheticisma <1071191953@qq.com>
2022-01-01 09:58:48 +0800

    2022_将一维数组转变成二维数组_easy

---
+++ cpp/2022_将一维数组转变成二维数组_easy.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if (m * n != original.size()) return {};
+        vector<vector<int>> ans(m, vector<int>(n));
+        for (int i = 0; i < original.size(); ++i)
+            ans[i/n][i%n] = original[i];
+        return ans;
+    }
+};
